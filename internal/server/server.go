ce334fbfc4a96ac4aeb9b3a0352a218a6bbbc4b6
zueve <zueves@gmail.com>
2021-12-28 23:59:43 +0200

    Support plain/text type

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/zueve/go-shortener/internal/services"
+	"io"
 	"net/http"
 	"strings"
 )
@@ -19,31 +20,44 @@ func New(service services.Service) Server {
 func (s *Server) Run() {
 	r := chi.NewRouter()
 	r.Post("/", s.createRedirect)
-	r.Get("/{key}", s.redirect)
+	r.Get("/{keyID}", s.redirect)
 	http.ListenAndServe(":8080", r)
 }
 
 func (s *Server) createRedirect(w http.ResponseWriter, r *http.Request) {
 	headerContentType := r.Header.Get("Content-Type")
 	w.Header().Set("content-type", "text/plain")
-	if headerContentType != "application/x-www-form-urlencoded" {
+	var url = ""
+	if headerContentType == "application/x-www-form-urlencoded" {
+		r.ParseForm()
+		url = r.FormValue("url")
+	} else if headerContentType == "text/plain; charset=utf-8" {
+		url_bytes, err := io.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusUnsupportedMediaType)
+			fmt.Println("invalid parse body")
+		}
+		url = string(url_bytes)
+	} else {
 		w.WriteHeader(http.StatusUnsupportedMediaType)
 		fmt.Println("invalid ContentType")
 		return
 	}
-	r.ParseForm()
-	url := r.FormValue("url")
+
 	if url == "" {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Println("invalid url")
 		return
 	}
+
 	key := s.service.CreateRedirect(url)
 	w.WriteHeader(http.StatusCreated)
 	w.Write([]byte(key))
 }
 
 func (s *Server) redirect(w http.ResponseWriter, r *http.Request) {
+	// TODO don't work
+	// key := chi.URLParam(r, "keyID")
 	key := strings.TrimPrefix(r.URL.Path, "/")
 	url, err := s.service.GetUrlByKey(key)
 	if err != nil {
