fc86fe3d9c6485d355658a89500b8499c1b96106
zueve <zueves@gmail.com>
2021-12-28 23:28:21 +0200

    Fix entrypoint v2

---
+++ cmd/shortener/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/zueve/go-shortener/internal/server"
+	"github.com/zueve/go-shortener/internal/services"
+	"github.com/zueve/go-shortener/internal/storage"
+)
+
+func main() {
+	var storage_ = storage.New()
+	var service_ = services.New(&storage_)
+	var server_ = server.New(service_)
+
+	server_.Run()
+}
