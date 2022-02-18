d40ff0f735c64985c7cc459ab64fc107efe48e08
zueve <zueves@gmail.com>
2022-02-18 21:14:47 +0200

    sprint3: fix interfaces

--- internal/services/interfaces.go
+++ internal/services/interfaces.go
@@ -2,7 +2,6 @@ package services
 
 import (
 	"context"
-	"fmt"
 )
 
 type StorageExpected interface {
@@ -11,24 +10,4 @@ type StorageExpected interface {
 	AddByBatch(ctx context.Context, urls []string, userID string) ([]string, error)
 	GetAllUserURLs(ctx context.Context, userID string) (map[string]string, error)
 	Ping(ctx context.Context) error
-}
-
-type LinkExistError struct {
-	Key string
-	Err error
-}
-
-func NewLinkExistError(key string, err error) error {
-	return &LinkExistError{
-		Key: key,
-		Err: err,
-	}
-}
-
-func (e *LinkExistError) Error() string {
-	return fmt.Sprintf("Link Already Exist %s, %s", e.Key, e.Err)
-}
-
-func (e *LinkExistError) Unwrap() error {
-	return e.Err
 }
